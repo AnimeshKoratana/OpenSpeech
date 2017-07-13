94283dc2a42e972e8194cf4f60f5bc8630197707
Animesh Koratana <animesh.koratana@gmail.com>
2017-07-13 09:34:48 -0400

    Sasi's Updates and fixes

--- KafkaAgent.py
+++ KafkaAgent.py
@@ -5,54 +5,74 @@ import client
 import RAKE
 import json
 import os
+import sys
 from pymongo import MongoClient
-
-
+from kafka.structs import OffsetAndMetadata
 
 def main():
+    os.system('sh /opt/run-task.sh');
     parser = argparse.ArgumentParser(description='Runs the Kafka Client to Transcribe Videos and Store the Output in MongoDB')
     parser.add_argument('--trans_server', type=str, help='Location of Kafka Producer',
                         default="ws://localhost:80/client/ws/speech")
     parser.add_argument('--kafka', type=str, help='Location of Kafka Producer', default="ec2-52-30-104-224.eu-west-1.compute.amazonaws.com")
-    parser.add_argument('--mp3_dir', type=str, help='Default is /content/audio', default="/content/audio")
-    parser.add_argument('--mp4_dir', type=str, help='Default is /content/video',  default="/content/video")
+    parser.add_argument('--mp3_dir', type=str, help='Default is /content/audio', default="/mnt/s3/content/audio/")
+    parser.add_argument('--mp4_dir', type=str, help='Default is /content/video',  default="/mnt/s3/content/video/")
     parser.add_argument('--debug', type=str, help='Just run on one input file')
-    args = parser.parse_args()
-    rake = RAKE.Rake('/opt/SmartStoplist.txt');
 
-    subprocess.call("sh /opt/start.sh -y /opt/models/english_nnet2.yaml", shell=True)
+
+    args = parser.parse_args()
+
+    rake = RAKE.Rake('SmartStoplist.txt');
+
+    subprocess.call("sh start.sh -y /opt/models/english_nnet2.yaml", shell=True)
 
     if not args.debug:
-        mongoclient = MongoClient(os.environ['MONGO_HOST'])
-        db = mongoclient[os.environ['MONGO_DB']].authenticate(os.environ['MONGO_USER'], os.environ['MONGO_PASS'])
-        collection = db[os.environ['COLLECTION']]
-        consumer = KafkaConsumer("manage-transcription", bootstrap_servers=args.kafka)
+        uri = "mongodb://"+os.environ['MONGO_USER']+":"+os.environ['MONGO_PASS']+"@"+os.environ['MONGO_HOST']+"/"+os.environ['MONGO_DB']+"?authMechanism=SCRAM-SHA-1"
+        mongoclient = MongoClient(uri)
+        db=mongoclient[os.environ['MONGO_DB']]
+        consumer = KafkaConsumer( bootstrap_servers=args.kafka, auto_offset_reset='earliest', group_id='media-transcription-group', max_poll_records=1, enable_auto_commit=False)
         while (True):
-            for input_msg in consumer:
-                file_path = get_file_path(input_msg.value, mp4dir=args.mp4_dir, mp3dir=args.mp3_dir)
-                try:
-                    if not file_path.__eq__("donothing"):
-                        output = run_pipeline(file_path=get_file_path(file_path, rake), trans_uri=args.trans_uri)
-                        collection.insert_one(output)
-                    else:
-                        print("create-transcription tag was not found in the input message...skipping")
-                except:
-                    print ("Error on " + input_msg.value + " ... Moving On")
+                consumer.subscribe(["media-transcription"])
+                records = consumer.poll(60000000,1)
+                consumer.commit()
+                consumer.unsubscribe()
+                print records
+                for rec in records:
+                        message = records[rec][0]
+                        #consumer.commit({rec: OffsetAndMetadata(message.offset, None)})
+                        print ("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                        file_path = get_file_path(message.value, mp4dir=args.mp4_dir, mp3dir=args.mp3_dir)
+                        try:
+                                if not file_path.__eq__("donothing"):
+                                        output = run_pipeline(file_path=file_path, rake=rake, trans_uri=args.trans_server)
+                                        mediafile=get_file_name(message.value)
+                                        text=json.loads(output)
+                                        db.Content.update({"mediaFileName" : mediafile},{"$set": {"transcriptedText" : text['transcription'], "transcription": output}}, upsert=False)
+                                        subprocess.call("python /opt/publish.py "+mediafile, shell=True)
+                                        print("Finished transcription and update for "+message.value+" ..")
+                                else:
+                                        print("create-transcription tag was not found in the input message...skipping")
+                        except:
+                                print ("Error on " + message.value + " ... Moving On")
+                                print sys.exc_info()
 
+                        print ("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
     else:
         run_pipeline(args.debug, rake=rake, trans_uri=args.trans_server)
-
 
 def run_pipeline(file_path, rake, trans_uri):
     print("Downsampling File and Cleaning Up Noise" + file_path)
     cleaned_file = downsample_audio(file_path)
     transcription = client.getTranscription(cleaned_file, uri=trans_uri)
+    print("Done with transcription")
+    #proc = subprocess.Popen(["python /opt/client.py -u="+trans_uri+" "+cleaned_file], stdout=subprocess.PIPE, shell=True)
+    #(transcription, err) = proc.communicate()
     write_transcription_file(transcription)
     keywords = rake.run(transcription)
     time_aligned = list(align_audio()['fragments'])
     final_json = createJSON(filepath=file_path, transcription=transcription, keywords=keywords, words=time_aligned)
     clean_for_next_iter()
-    print final_json
+    print "keywords done"
     return final_json
 
 
@@ -88,25 +108,30 @@ def clean_for_next_iter():
 
 
 def downsample_audio(path_to_file):
+    #subprocess.call("rm -rf /tmp/openspeech/transformed.wav", shell=True)
     downsample_command = ['ffmpeg' , '-i' , path_to_file, '-qscale', '0', '-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16000', '/tmp/openspeech/transformed.wav']
     subprocess.Popen(downsample_command).wait()
     return "/tmp/openspeech/transformed.wav"
-
-
 
 def get_file_path(input_message, mp4dir, mp3dir):
     if str(input_message).__contains__("create-transcription"):
         path = str(str(input_message).split("##")[1])
         fullpath = ""
         if (path.endswith('.mp4') or path.endswith('.m4v')):
-            fullpath += mp4dir
+            fullpath=mp4dir
         elif path.endswith('.mp3'):
-            fullpath += mp3dir
-        fullpath += path
-        return path
+            fullpath=mp3dir
+        fullpath+=path
+        return fullpath
     else:
         return "donothing"
 
+def get_file_name(input_message):
+    if str(input_message).__contains__("create-transcription"):
+        path = str(str(input_message).split("##")[1])
+        return path
+    else:
+        return "donothing"
 
 if __name__ == '__main__':
     main()
--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import argparse
 from ws4py.client.threadedclient import WebSocketClient
+import time
 import threading
 import sys
 import urllib
@@ -92,7 +93,6 @@ class MyClient(WebSocketClient):
             if 'message' in response:
                 print >> sys.stderr, "Error message:",  response['message']
 
-
     def get_full_hyp(self, timeout=60):
         return self.final_hyp_queue.get(timeout)
 
@@ -108,7 +108,6 @@ class MyClient(WebSocketClient):
         self.close(code=1006, reason=" ")
         time.sleep(0.2)
 
-
 def getTranscription(audiofile, uri ="ws://localhost:8080/client/ws/speech", rate = 32000, content_type = ''):
     if content_type == '' and audiofile.endswith(".raw"):
         content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)%d, format=(string)S16LE, channels=(int)1" % (rate / 2)
@@ -118,19 +117,19 @@ def getTranscription(audiofile, uri ="ws://localhost:8080/client/ws/speech", rat
     ws.connect()
     result = ws.get_full_hyp()
     print result.encode('utf-8')
+
     final_transcription = ""
     for line in list(ws.gettrans()):
         final_transcription += line
         final_transcription += " "
-    ws.clean_ws()
+   # ws.clean_ws()
     print str(ws.final_hyp_queue)
     return final_transcription
 
 
 def main():
-
     parser = argparse.ArgumentParser(description='Command line client for kaldigstserver')
-    parser.add_argument('-u', '--uri', default="ws://localhost:8080/client/ws/speech", dest="uri", help="Server websocket URI")
+    parser.add_argument('-u', '--uri', default="ws://localhost:80/client/ws/speech", dest="uri", help="Server websocket URI")
     parser.add_argument('-r', '--rate', default=32000, dest="rate", type=int, help="Rate in bytes/sec at which audio should be sent to the server. NB! For raw 16-bit audio it must be 2*samplerate!")
     parser.add_argument('--save-adaptation-state', help="Save adaptation state to file")
     parser.add_argument('--send-adaptation-state', help="Send adaptation state from file")
@@ -142,22 +141,19 @@ def main():
     if content_type == '' and args.audiofile.endswith(".raw"):
         content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)%d, format=(string)S16LE, channels=(int)1" %(args.rate/2)
 
-
-
     ws = MyClient(args.audiofile, args.uri + '?%s' % (urllib.urlencode([("content-type", content_type)])), byterate=args.rate,
                   save_adaptation_state_filename=args.save_adaptation_state, send_adaptation_state_filename=args.send_adaptation_state)
     ws.connect()
     result = ws.get_full_hyp()
     print result.encode('utf-8')
-
+#    ws.close()
+    #time.sleep(0.2)
     final_transcription = ""
     for line in list(ws.gettrans()):
         final_transcription += line
         final_transcription += " "
     print final_transcription
-
-
-
+    #ws.clean_ws()
 
 if __name__ == "__main__":
     main()
